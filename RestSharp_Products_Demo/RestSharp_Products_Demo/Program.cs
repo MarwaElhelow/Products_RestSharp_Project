ab6f34db7cd899df35a078cafb16e150d93d57c9
MarwaElhelow <78296413+MarwaElhelow@users.noreply.github.com>
2023-08-31 14:16:18 +0300

    Add files via upload

---
+++ RestSharp_Products_Demo/RestSharp_Products_Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using NUnit;
+using NUnit.Framework;
+using System.Net;
+
+namespace RestSharp_FirstProject
+{
+    public class RestSharpRunner
+    {
+        public static void Main(string[] args)
+        {
+            // Precondition Initialization
+            var baseUrl = "https://dummyjson.com/products";
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest();
+
+            Console.WriteLine($"{baseUrl} {request.Method}");
+
+            var response = client.Execute(request);
+
+            Console.WriteLine($"Response Content: {response.Content}");
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+
+}
